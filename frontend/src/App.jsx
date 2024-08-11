38f74a80d1290d1c7e392948663e3fb39a3053ee
Anurag Kandulna <dev.ak@outlook.in>
2024-08-12 00:44:24 +0530

    added integration code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,16 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Form from './components/Form'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
+      <h1 className="text-3xl font-bold text-center">
+      Simple User Form
       </h1>
+      <Form />
     </>
   )
 }
