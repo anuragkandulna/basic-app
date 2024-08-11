786a0578a987d917679ccac62ef2435a043ff57e
Anurag Kandulna <dev.ak@outlook.in>
2024-08-11 17:48:10 +0530

    added shell function to install requirements

--- setup_python_venv.sh
+++ setup_python_venv.sh
@@ -1,44 +1,75 @@
 #!/usr/bin/env bash
 
+
 # Function to check if python 3 is installed
 check_python_installed() {
-  if command -v python3 &> /dev/null
-  then
-    echo "Python 3 is already installed. Version: $(python3 --version)"
-  elif command -v python &> /dev/null
-  then
-    echo "Python 3 is already installed. Version: $(python --version)"
-  else
-    echo "Python 3 is not installed. Please install Python 3."
-    exit 1
-  fi
+    if command -v python3 &> /dev/null
+    then
+        echo "Python 3 is already installed. Version: $(python3 --version)"
+    elif command -v python &> /dev/null
+    then
+        echo "Python 3 is already installed. Version: $(python --version)"
+    else
+        echo "Python 3 is not installed. Please install Python 3."
+        exit 1
+    fi
 }
 
-create_virtualenv() {
-  # Cleaning up local v-environment
-  if [ -d $ENV ]; then
-    echo "Python Virtual environment ${ENV} already exist. Cleaning up the environment..."
-    rm -rf "${ENV}/"
-    echo "Successfully cleared the old environment..."
-  fi
 
-  # Creating a new environment
-  echo "Creating new virtual environment..."
-  if command -v python3 &> /dev/null
-  then
-    python3 -m venv $ENV
-  else
-    python -m venv $ENV
-  fi
+# Function to create Python 3 virtual environment
+create_python_venv() {
+    # Cleaning up local v-environment
+    if [ -d $ENV ]; then
+        echo "Python Virtual environment ${ENV} already exist. Cleaning up the environment..."
+        rm -rf "${ENV}/"
+        echo "Successfully cleared the old environment..."
+    fi
 
-  # Verify environment creation was success
-  if [ $? -eq 0 ]; then
-    echo "Successfully created virtual environment in directory: ${ENV}"
-  else
-    echo "Failed to create virtual environment. Exiting now!"
-    exit 1
-  fi
+    # Creating a new environment
+    echo "Creating new virtual environment..."
+    if command -v python3 &> /dev/null
+    then
+        python3 -m venv $ENV
+    else
+        python -m venv $ENV
+    fi
 
+    # Verify environment creation was success
+    if [ $? -eq 0 ]; then
+        echo "Successfully created virtual environment in directory: ${ENV}"
+    else
+        echo "Failed to create virtual environment. Exiting now!"
+        exit 1
+    fi
+
+}
+
+
+# Function to activate Python 3 venv and update packages
+update_python_packages() {
+    # Check if .pyenv312 directory exists
+    if [ -d $ENV ]; then
+        echo "Python Virtual environment ${ENV} exist. Updating packages..."
+    else
+        echo "No ${ENV} python virtual environment found. Exiting now!"
+        exit 1
+    fi
+
+    # Activate the virtual environment
+    source $ENV/bin/activate
+
+    # Install the packages from requirements_3_12.txt
+    if [ -f "requirements.txt" ]; then
+        pip install -r $REQUIREMENTS_FILE
+        echo "Python packages installed from ${REQUIREMENTS_FILE} ..."
+    else
+        echo "${REQUIREMENTS_FILE} not found. Exiting now!"
+        exit 1
+    fi
+
+    # Deactivate the python virtual environment
+    deactivate
+    echo "Python virtual environment deactivated. Success!"
 }
 
 
@@ -46,10 +77,12 @@ create_virtualenv() {
 # Start python virtual environment creation with hidden dir .pyenv312
 ENV=".pyenv312"
 GIT_IGNORE=".gitignore"
+REQUIREMENTS_FILE="requirements_3_12.txt"
 
 # Invoke functions to do the respective task
 check_python_installed
-create_virtualenv
+create_python_venv
+update_python_packages
 
 # Add ENV to .gitignore
 if grep -Fxq "${ENV}" $GIT_IGNORE
